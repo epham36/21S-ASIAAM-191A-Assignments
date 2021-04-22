c8e478533888b741604dca15c3329a289392ddb7
epham36 <emilypham36@gmail.com>
2021-04-22 11:06:25 -0700

    lab 3

---
+++ Week_03/js/init.js
@@ -0,0 +1,40 @@
+// declare variables
+let zoomLevel = 5;
+const mapCenter = [34.0709,-118.444];
+
+// use the variables
+const map = L.map('map').setView(mapCenter, zoomLevel);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// create a function to add markers
+function addMarker(lat,lng,title,message){
+    console.log(message)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2>`)
+    createButtons(lat,lng,title); // new line!!!
+    return message
+}
+
+// create a function to add buttons with a fly to command
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+
+    // attach an event listner to the button with Leaflet's map.flyTo
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng]); 
+    })
+    document.body.appendChild(newButton); //this adds the button to our page.
+}
+
+// use our marker functions
+addMarker(37.20048562083675, -112.98934000215036,'Zion National Park','Winter 2018/2019')
+addMarker(38.73578420068832, -109.5952890277475,'Arches National Park','Winter 2018/2019')
+addMarker(36.488114358399194, -118.5639215319455,'Sequoia National Park','Winter 2017/2018')
+addMarker(38.27761884764569, -120.30941621931653,'Calaveras Big Trees State Park','Winter 2017/2018')
+addMarker(36.10935437791291, -112.19551391305926, 'Grand Canyon National Park', 'I dont remember when I went here')
